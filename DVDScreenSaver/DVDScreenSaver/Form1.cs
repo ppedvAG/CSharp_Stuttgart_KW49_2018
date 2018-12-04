5e1d2d4eba5abb30abf4b0be6a915993b75e62ba
André <andrerrr@outlook.de>
2018-12-04 14:14:43 +0100

    DVD Screens sind gesaved

---
+++ DVDScreenSaver/DVDScreenSaver/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVDScreenSaver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int speedX = 2;
+        int speedY = 2;
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (button1.Right >= ClientRectangle.Right || button1.Left <= 0)
+            {
+                speedX *= -1;
+                ChangeColor();
+            }
+
+            if (button1.Bottom >= ClientRectangle.Bottom || button1.Top <= 0)
+            {
+                speedY *= -1;
+                ChangeColor();
+            }
+
+            button1.Left += speedX;
+            button1.Top += speedY;
+
+            Text = $"{button1.Left}x{button1.Top}";
+        }
+        Random ran = new Random();
+        private void ChangeColor()
+        {
+            Task.Run(() => Console.Beep());
+            button1.BackColor = GetRandomColor();
+            BackColor = GetRandomColor();
+        }
+
+        private Color GetRandomColor()
+        {
+            return Color.FromArgb(ran.Next(0, 255), ran.Next(0, 255), ran.Next(0, 255));
+        }
+    }
+}
