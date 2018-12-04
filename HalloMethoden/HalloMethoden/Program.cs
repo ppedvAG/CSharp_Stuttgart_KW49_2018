4aeebda24337ef12fd174ababf3b3518f7955eff
André <andrerrr@outlook.de>
2018-12-04 15:14:16 +0100

    Methoden methodik

---
+++ HalloMethoden/HalloMethoden/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloMethoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Methoden und Funktionen");
+
+            SagHallo();
+            int eineZahl = GetZahl();
+
+            Console.WriteLine($"Die Zahl ist {eineZahl}");
+            Console.ReadLine();
+        }
+
+        static void SagHallo()
+        {
+            ZeigeText("Hallo");
+        }
+
+        static void ZeigeText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static int GetZahl()
+        {
+            return 5;
+        }
+    }
+}
