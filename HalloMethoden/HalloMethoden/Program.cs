09a771c818517d7726914ad04afe16e900488be8
André <andrerrr@outlook.de>
2018-12-05 12:03:09 +0100

    Überladung

--- HalloMethoden/HalloMethoden/Program.cs
+++ HalloMethoden/HalloMethoden/Program.cs
@@ -14,24 +14,43 @@ namespace HalloMethoden
 
             SagHallo();
             int eineZahl = GetZahl();
-
+            ZeigeText(888);
             Console.WriteLine($"Die Zahl ist {eineZahl}");
             Console.ReadLine();
         }
 
         static void SagHallo()
         {
-            ZeigeText("Hallo");
+            ZeigeText($"Hallo {DateTime.Now:T}");
+            ZeigeText("Temp:", 7);
         }
 
         static void ZeigeText(string text)
         {
-            Console.WriteLine(text);
+            ZeigeText(text, 12);
+        }
+
+        static void ZeigeText(string text, int zahl)
+        {
+            Console.WriteLine($"{text}:{zahl}");
+        }
+
+        static void ZeigeText(int zahl)
+        {
+            ZeigeText("--",zahl);
         }
 
         static int GetZahl()
         {
             return 5;
+        }
+
+        static int Verdoppeln(int zahl)
+        {
+            if (zahl > 1000)
+                return zahl;
+
+            return zahl * 2;
         }
     }
 }
